bc6e7d35489ea1cb8488559b2e8c0c7ec63d24b9
Matt Lane <lane.matthew@gmail.com>
2019-05-17 15:57:52 -0700

    building Joke component

--- src/Joke.js
+++ src/Joke.js
@@ -1,8 +1,28 @@
 import React, { Component } from "react";
+import "./Joke.css";
 
 class Joke extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      punchlineVisible: false
+    };
+    this.togglePunchline = this.togglePunchline.bind(this);
+  }
+
+  togglePunchline() {
+    this.setState(st => ({ punchlineVisible: !st.punchlineVisible }));
+  }
+
   render() {
-    return <h1>Joke</h1>
+    const { setup, punchline } = this.props.joke;
+    const { punchlineVisible } = this.state;
+    return (
+      <div className="Joke" onClick={this.togglePunchline}>
+        <h3>{setup}</h3>
+        <p className={!punchlineVisible && "prompt"}>{punchlineVisible ? punchline : "Click to reveal punchline!"}</p>
+      </div>
+    );
   }
 }
 
